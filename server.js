576116d51e50b38706283c051c838a1c4c6bfab2
Manishmsn <mk065005@gmail.com>
2025-01-02 22:21:55 +0530

    Adding - log handler

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
-  next();
-});
+app.use(logger);
+
+// Cross Origin Resource sharing
+const whitelist = [
+  "https://www.google.co.in/",
+  "https://www.google.co.in",
+  "https://expressjs.co",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== 1) {
+      callback(null, true);
+    } else {
+      callback(new Error(`Not allowed by cors`));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
 // in other words, from data:
@@ -70,5 +89,7 @@ app.get("/chain(.html)?", [one, two, three]);
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
